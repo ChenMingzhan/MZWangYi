dc1ec43376050078660d858b2ec3a568c4151d8a
MZMZWangYi <785089158@qq.com>
2016-04-13 15:40:19 +0800

    初始化

---
+++ WangYiTest/WangYiTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WangYiTest
+//
+//  Created by 铭铭 on 16/4/13.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WangYiTest/WangYiTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WangYiTest
+//
+//  Created by 铭铭 on 16/4/13.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
